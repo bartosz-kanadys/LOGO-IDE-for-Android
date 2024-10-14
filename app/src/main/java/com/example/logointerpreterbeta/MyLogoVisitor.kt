b570c612578c69f81c152f2b7cc1aea1493de007
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-10-14 21:26:52 +0200

    Add new logo commands to visitor
    repeat, random, colors...

--- app/src/main/java/com/example/logointerpreterbeta/MyLogoVisitor.kt
+++ app/src/main/java/com/example/logointerpreterbeta/MyLogoVisitor.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.RectF
 import com.example.logointerpreterbeta.interpreter.logoBaseVisitor
 import com.example.logointerpreterbeta.interpreter.logoParser
 import kotlin.math.cos
@@ -18,10 +19,11 @@ class MyLogoVisitor : logoBaseVisitor<Int>() {
         // Ustawienia malowania (kolor, grubość linii)
         paint.color = Color.BLACK
         paint.strokeWidth = 5f
+        paint.style = Paint.Style.STROKE
     }
 
     override fun visitFd(ctx: logoParser.FdContext?): Int {
-        val distance = ctx!!.expression().text.toInt()
+        val distance = visit(ctx!!.expression())
         // Konwersja kąta na radiany
         val angleRadians = Math.toRadians((Turtle.direction - 90).toDouble())
 
@@ -37,7 +39,7 @@ class MyLogoVisitor : logoBaseVisitor<Int>() {
     }
 
     override fun visitBk(ctx: logoParser.BkContext?): Int {
-        val distance = ctx!!.expression().text.toInt()
+        val distance = visit(ctx!!.expression())
         // Konwersja kąta na radiany
         val angleRadians = Math.toRadians((Turtle.direction - 90 - 180).toDouble())
 
@@ -52,16 +54,29 @@ class MyLogoVisitor : logoBaseVisitor<Int>() {
         return 0
     }
 
+    override fun visitArc(ctx: logoParser.ArcContext?): Int {
+        val angle = visit(ctx!!.expression(0))!!.toFloat()
+        val distance = visit(ctx.expression(1))
+
+        val rectF = RectF(
+            Turtle.Xposition-distance,
+            Turtle.Yposition-distance,
+            Turtle.Xposition+distance,
+            Turtle.Yposition+distance
+        )
+
+        canvas.drawArc(rectF,270f+Turtle.direction, angle,false,paint)
+        return 0
+    }
+
     override fun visitRt(ctx: logoParser.RtContext?): Int{
-        val exp = ctx!!.expression()
-        val angle = exp!!.text.toInt()
+        val angle =visit(ctx!!.expression())
         Turtle.direction += angle
         return 0
     }
 
     override fun visitLt(ctx: logoParser.LtContext?): Int{
-        val exp = ctx!!.expression()
-        val angle = exp!!.text.toInt()
+        val angle = visit(ctx!!.expression())
         Turtle.direction -= angle
         return 0
     }
@@ -87,11 +102,69 @@ class MyLogoVisitor : logoBaseVisitor<Int>() {
     }
 
     override fun visitSetxy(ctx: logoParser.SetxyContext?): Int {
-        val X = ctx!!.expression(0).text.toFloat()
-        val Y = ctx.expression(1).text.toFloat()
-        Turtle.setAcctualPosition(X,Y)
+        val x = ctx!!.expression(0).text.toFloat()
+        val y = ctx.expression(1).text.toFloat()
+        Turtle.setAcctualPosition(x,y)
         return 0
     }
 
+    override fun visitRepeat_(ctx: logoParser.Repeat_Context?): Int {
+        val repeatCount = ctx!!.number().text.toInt()
+        val commandsBlock = ctx.block().children
+
+        for (i in 1..repeatCount) {
+            for (command in commandsBlock) {
+                visit(command)
+            }
+        }
+        return 0
+    }
+
+    override fun visitSetpencolor(ctx: logoParser.SetpencolorContext?): Int {
+        val intColor = visit(ctx!!.expression())
+        val color = penColors[intColor]
+        paint.setColor(color)
+        Turtle.penColor = color
+        return 0
+    }
+
+    override fun visitSetpensize(ctx: logoParser.SetpensizeContext?): Int {
+        val size = visit(ctx!!.expression())
+        paint.strokeWidth = size.toFloat()
+        Turtle.penSize = size
+        return 0
+    }
+
+    override fun visitSetscreencolor(ctx: logoParser.SetscreencolorContext?): Int {
+        val intColor = visit(ctx!!.expression())
+        val color = penColors[intColor]
+        canvas.drawColor(color)
+        return 0
+    }
+
+    override fun visitExpression(ctx: logoParser.ExpressionContext?): Int {
+        return visitChildren(ctx) // Przekazuje do dzieci, żeby w pełni odwiedzić drzewo wyrażeń
+    }
+
+    override fun visitSignExpression(ctx: logoParser.SignExpressionContext?): Int {
+        if (ctx!!.func_() != null) {
+            return visit(ctx.func_())
+        }
+
+        if (ctx.number() != null) {
+            return ctx.number().text.toInt() // Zwraca liczbę
+        }
+
+        if (ctx.deref() != null) {
+            return visit(ctx.deref()) // Zwraca wartość zmiennej
+        }
+
+        return 0 // Domyślna wartość, gdy nic nie pasuje
+    }
+
+    override fun visitRandom(ctx: logoParser.RandomContext?): Int {
+        val maxValue = visit(ctx!!.expression())!!
+        return (0..maxValue-1).random()
+    }
    
 }
