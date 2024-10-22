a38b283c35fa14ad8717d7a9a8aa03716abc08d3
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-10-22 22:34:58 +0200

    Code cleaning

--- app/src/main/java/com/example/logointerpreterbeta/MainAppActivity.kt
+++ app/src/main/java/com/example/logointerpreterbeta/MainAppActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -24,7 +23,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -36,10 +34,8 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.logointerpreterbeta.errors.MyErrorListener
 import com.example.logointerpreterbeta.errors.SyntaxError
 import com.example.logointerpreterbeta.ui.theme.LogoInterpreterBetaTheme
-import androidx.compose.ui.tooling.preview.Preview as Preview
 
 
 class MainAppActivity: ComponentActivity() {
