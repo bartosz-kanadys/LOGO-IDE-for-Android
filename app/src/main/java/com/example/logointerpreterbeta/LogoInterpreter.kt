91bf68c0ac93206100619ac96773f778560ebb92
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-10-22 22:32:40 +0200

    Code cleaning

--- app/src/main/java/com/example/logointerpreterbeta/LogoInterpreter.kt
+++ app/src/main/java/com/example/logointerpreterbeta/LogoInterpreter.kt
@@ -1,41 +1,37 @@
 package com.example.logointerpreterbeta
 
 import android.graphics.Bitmap
+import com.example.logointerpreterbeta.errors.MyErrorListener
 import com.example.logointerpreterbeta.interpreter.logoLexer
 import com.example.logointerpreterbeta.interpreter.logoParser
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 
 class LogoInterpreter {
-
     var bitmap:Bitmap? = null
 
-    //"fd 100 rt 90 penup fd 100 rt 90 pendown fd 100 rt 90 penup fd 100";
-
-//    private var walker =  ParseTreeWalker()
-//    private var listener = MyLogoListener()
+    val myVisitor = MyLogoVisitor()
 
     fun start(input: String) {
         // Tworzenie lexer'a
         val lexer = logoLexer(
-            CharStreams.fromString(input)
+            CharStreams.fromString(input+"\n")
         )
+        lexer.removeErrorListeners()
+        lexer.addErrorListener(MyErrorListener())
 
         // Tokenizacja
         val tokens = CommonTokenStream(lexer)
 
         // Parsowanie
         val parser = logoParser(tokens)
-
+        parser.removeErrorListeners()
+        parser.addErrorListener(MyErrorListener())
         // Startujemy od reguły głównej (prog)
         val tree = parser.prog()
 
-        val myVisitor = MyLogoVisitor()
         myVisitor.visit(tree)
         bitmap = myVisitor.image
-
-//        walker.walk(listener, tree)
-//        bitmap = listener.image
     }
 
 }
