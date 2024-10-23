97f0521abd3c0387f180bc361da2d08d9de69758
Bartosz Kanadys <kanadysbartosz@gmail.com>
2024-10-23 21:34:50 +0200

    Refactor Bitmap class to Image Settings

---
+++ app/src/main/java/com/example/logointerpreterbeta/ImageSettings.kt
@@ -0,0 +1,9 @@
+package com.example.logointerpreterbeta
+
+import android.graphics.Bitmap
+
+val MyImageWidth = 2000
+val MyImageHeight = 2000
+
+var MyImage: Bitmap = Bitmap.createBitmap(MyImageWidth, MyImageHeight, Bitmap.Config.ARGB_8888)
+
